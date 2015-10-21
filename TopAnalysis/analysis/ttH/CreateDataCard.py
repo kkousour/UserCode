ff9a276de852fcb26efbb8d8c8862bc3a21adf02
isildak <bora.isildak@cern.ch>
2015-10-21 16:15:35 +0300

    Bkg normalizations added.

--- TopAnalysis/analysis/ttH/CreateDataCard.py
+++ TopAnalysis/analysis/ttH/CreateDataCard.py
@@ -1,8 +1,8 @@
 #!usr/bin/python
 import ROOT
-from ROOT import TH1F, TCanvas, TFile, gROOT, TTree, TCut, TMath
+from ROOT import TH1F, TCanvas, TFile, gROOT, TTree, TCut, TMath, RooRealVar, RooDataHist, RooArgList, RooArgSet, RooAddPdf, RooFit, RooGenericPdf, RooWorkspace, RooMsgService, RooHistPdf
 
-gROOT.ForceStyle()		
+gROOT.ForceStyle()    
 
 FileName = ["ttHJetTobb_M125", "TT", "QCD_HT200to300", "QCD_HT300to500", "QCD_HT500to700", "QCD_HT700to1000", "QCD_HT1000to1500", "QCD_HT1500to2000", "QCD_HT2000toInf"]
 
@@ -17,23 +17,24 @@ QCD_HT1000to1500_xsec = 1.06e+03
 QCD_HT1500to2000_xsec = 1.22e+02
 QCD_HT2000toInf_xsec  = 2.54e+01
 
-XSEC = [ttH_xsec*Hbb_BR, ttbar_xsec, QCD_HT200to300_xsec, QCD_HT300to500_xsec, QCD_HT500to700_xsec, QCD_HT700to1000_sec, QCD_HT1000to1500_xsec, QCD_HT1500to2000_xsec, QCD_HT2000toInf_xsec]	
+XSEC = [ttH_xsec*Hbb_BR, ttbar_xsec, QCD_HT200to300_xsec, QCD_HT300to500_xsec, QCD_HT500to700_xsec, QCD_HT700to1000_sec, QCD_HT1000to1500_xsec, QCD_HT1500to2000_xsec, QCD_HT2000toInf_xsec]  
 
 print FileName, XSEC
 
-LUMI = 10000		
-NCAT = 2		
-inf  = []		
-tr   = []	
-h = [[] for x in range(len(FileName))] 
+LUMI  = 595   
+NCAT  = 2   
+inf   = []    
+tr    = []  
+h     = [[] for x in range(len(FileName))] 
+hData = []
 
-COLOR = [ROOT.kRed, ROOT.kBlack, ROOT.kBlue, ROOT.kBlue-5, ROOT.kBlue-8, ROOT.kGreen, ROOT.kGreen-4, ROOT.kGray, ROOT.kRed+4]				
-CUT = []		
-CUT.append(TCut("ht>500 && jetPt[5]>40 && nBJets==2"))		
-CUT.append(TCut("ht>500 && jetPt[5]>40 && nBJets>2"))		
+dataf = TFile.Open("flatTree_JetHT.root")  # data file
 
-can = TCanvas("Canvas0","Canvas0",600,600)
-can.Divide(2,1)	
+COLOR = [ROOT.kRed, ROOT.kBlack, ROOT.kBlue, ROOT.kBlue-5, ROOT.kBlue-8, ROOT.kGreen, ROOT.kGreen-4, ROOT.kGray, ROOT.kRed+4]       
+CUT   = [TCut("ht>500 && jetPt[5]>40 && nBJets==2"), TCut("ht>500 && jetPt[5]>40 && nBJets>2")]   
+
+can = TCanvas("Canvas0","Canvas0", 600, 600)
+can.Divide(2,1) 
 
 for i in xrange(len(FileName)):
   inf.append(TFile.Open("flatTree_" + FileName[i] + ".root"))
@@ -44,14 +45,14 @@ for i in xrange(len(FileName)):
   for k in xrange(NCAT):
     can.cd(k + 1)
     print i, k
-    h[i].append(TH1F("h" + str(i) + str(k), "h" + str(i) + str(k), 25,-1,1))	
+    h[i].append(TH1F("h" + str(i) + str(k), "h" + str(i) + str(k), 25,-1,1))  
     h[i][k].Sumw2()
     tr[i].Draw("mva>>"  +  "h" + str(i) + str(k), CUT[k])
     h[i][k].SetLineWidth(2)
     h[i][k].SetLineColor(COLOR[i])
-    h[i][k].Scale(LUMI*XSEC[i]/hpu.GetEntries()) 		
+    h[i][k].Scale(LUMI*XSEC[i]/hpu.GetEntries())    
 
-outf = TFile("ttH-shapes.root","RECREATE")		
+outf = TFile("ttH-shapes.root", "RECREATE")   
 hQCD = []
 
 for k in xrange(NCAT):
@@ -63,80 +64,129 @@ for k in xrange(NCAT):
   h[0][k].SetFillColor(ROOT.kRed-10)
   h[1][k].SetFillColor(ROOT.kGreen-10)
   hQCD[k].SetFillColor(ROOT.kBlue-10)
-  h[0][k].SetFillStyle(3001)	
+  h[0][k].SetFillStyle(3001)  
   h[1][k].SetFillStyle(3001)
   print "QCD events:   ", hQCD[k].Integral()
   print "TTbar events: ", h[1][k].Integral()
-  print "TTH events:   ", h[0][k].Integral()		
+  print "TTH events:   ", h[0][k].Integral()    
 
-  max1 = TMath.Max(h[1][k].GetBinContent(h[1][k].GetMaximumBin()), hQCD[k].GetBinContent(hQCD[k].GetMaximumBin()))	
+  max1 = TMath.Max(h[1][k].GetBinContent(h[1][k].GetMaximumBin()), hQCD[k].GetBinContent(hQCD[k].GetMaximumBin()))  
   max2 = TMath.Max(h[0][k].GetBinContent(h[0][k].GetMaximumBin()), max1)
   hQCD[k].SetMaximum(1.1*max2)
   hQCD[k].Draw("hist")
   h[0][k].Draw("same hist")
   h[1][k].Draw("same hist")
 
-  outf.cd()
   hQCD[k].SetName("qcdCAT"      + str(k))
-  hQCD[k].Write()
 
   h[0][k].SetName("signalCAT"   + str(k))
-  h[0][k].Write()
 
   h[1][k].SetName("ttjetsCAT"   + str(k))
-  h[1][k].Write()
 
-  hQCD[k].SetName("data_obsCAT" + str(k)) 
-  hQCD[k].Write()	
+  dataf.cd()
+  hData.append(TH1F("data_obsCAT" + str(k), "data_obsCAT" + str(k), 25, -1, 1))
+  datatree = dataf.Get("hadtop/events")
+  hpu = dataf.Get("hadtop/pileup")
+  datatree.Draw("mva>>"  +  hData[k].GetName(), CUT[k])
 
-datacard = open("datacard_ttH.txt","w")	
-datacard.write("imax "  + str(NCAT) + "\n")		
-datacard.write("jmax *" + "\n")		
-datacard.write("kmax *" + "\n")		
-datacard.write(7*12*"-" + "\n")		
-datacard.write("shapes *  * " + outf.GetName() + " $PROCESS$CHANNEL" + "\n")		
-datacard.write(7*12*"-" + "\n")
-datacard.write("bin         ")		
+workspace   = RooWorkspace("w","workspace")
+RooSigHist  = []
+MVA         = [] 
+RooObsHist  = []
+RooSigHist  = []
+RooQCDHist  = [] 
+RooTTJHist  = [] 
+ObsPDF      = []
+SignalPDF   = []
+QCDPDF      = []
+TTJPDF      = []
+QCD_Norm = []  
+TTJ_Norm = []  
 
 for k in xrange(NCAT):
-  name = "CAT"+str(k) + " "	
+  
+  MVA.append(RooRealVar("mva","mva", -1.0, 1.0))
+  
+  RooObsHist.append(RooDataHist("RooObsHist", "RooObsHist", RooArgList(MVA[-1]), hData[k]))
+  RooSigHist.append(RooDataHist("RooSigHist", "RooSigHist", RooArgList(MVA[-1]), h[0][k]))
+  RooQCDHist.append(RooDataHist("RooQCDHist", "RooQCDHist", RooArgList(MVA[-1]), hQCD[k]))
+  RooTTJHist.append(RooDataHist("RooTTJHist", "RooTTJHist", RooArgList(MVA[-1]), h[1][k]))
+  RooObsHist[-1].Print()
+  
+  SignalPDF.append(RooHistPdf( "signalCAT"   + str(k), "signalCAT"   + str(k), RooArgSet(MVA[-1]), RooSigHist[-1]))
+  QCDPDF.append(RooHistPdf(    "qcdCAT"      + str(k), "qcdCAT"      + str(k), RooArgSet(MVA[-1]), RooQCDHist[-1]))
+  TTJPDF.append(RooHistPdf(    "ttjetsCAT"   + str(k), "ttjetsCAT"   + str(k), RooArgSet(MVA[-1]), RooTTJHist[-1]))
+  QCD_Norm.append(RooRealVar("QCD_Norm","QCD_Norm", 0, -1e+04, 1e+04))
+  TTJ_Norm.append(RooRealVar("TTJ_Norm","TTJ_Norm", 0, -1e+04, 1e+04))
+  
+  getattr(workspace,'import')(RooObsHist[-1], RooFit.Rename("data_obsCAT" + str(k)))
+  getattr(workspace,'import')(SignalPDF[-1])
+  getattr(workspace,'import')(QCDPDF[-1])
+  getattr(workspace,'import')(TTJPDF[-1])
+  getattr(workspace,'import')(QCD_Norm[-1])
+  getattr(workspace,'import')(TTJ_Norm[-1])
+  
+workspace.Print()
+workspace.writeToFile("ttH-shapes.root")
+
+datacard = open("datacard_ttH.txt","w") 
+datacard.write("imax "  + str(NCAT) + "\n")   
+datacard.write("jmax *" + "\n")   
+datacard.write("kmax *" + "\n")   
+datacard.write(7*12*"-" + "\n")   
+datacard.write("shapes *  * " + outf.GetName() + " w:$PROCESS$CHANNEL" + "\n")    
+datacard.write(7*12*"-" + "\n")
+datacard.write("{:<12}".format("bin"))    
+
+for k in xrange(NCAT):
+  name = "CAT"+str(k) + " " 
+  datacard.write("{:<12}".format(name))
+
+datacard.write("\n")    
+datacard.write("{:<12}".format("observation "))
+
+for k in xrange(NCAT):
+  nevents_observed = hData[k].Integral()
+  print "Number of events observed: ", nevents_observed
+  datacard.write("{:<12}".format(str(nevents_observed)))
+
+datacard.write("\n")
+datacard.write(7*12*"-" + "\n")
+datacard.write("{:<12}".format("bin"))
+
+for k in xrange(NCAT):
+  name = "{:>12} {:>12} {:>12}".format("CAT" + str(k), "CAT" + str(k), "CAT" + str(k))
   datacard.write(name)
 
-datacard.write("\n")		
-datacard.write("observation ")
+datacard.write("\n")
+datacard.write("{:<12}".format("process"))
 
 for k in xrange(NCAT):
-  datacard.write("-1 ")
+  datacard.write("{:>12} {:>12} {:>12}".format("signal", "ttjets", "qcd"))
 
 datacard.write("\n")
-datacard.write(7*12*"-" + "\n")
-datacard.write('{:<12}'.format("bin"))
-
-for k in xrange(NCAT):
-  name = '{:>12} {:>12} {:>12}'.format("CAT" + str(k), "CAT" + str(k), "CAT" + str(k))
-  datacard.write(name)
-
-datacard.write("\n")
-datacard.write('{:<12}'.format("process"))
-
-for k in xrange(NCAT):
-  datacard.write('{:>12} {:>12} {:>12}'.format("signal", "ttjets", "qcd"))
-
-datacard.write("\n")
-datacard.write('{:<12}'.format("process"))
+datacard.write("{:<12}".format("process"))
  
 for k in xrange(NCAT):
-  datacard.write('{:>12} {:>12} {:>12}'.format("0", "1", "2"))
+  datacard.write("{:>12} {:>12} {:>12}".format("0", "1", "2"))
 
 datacard.write("\n")
-datacard.write('{:<12}'.format("rate"))
+datacard.write("{:<12}".format("rate"))
 
 for k in xrange(NCAT):
   rate1 = h[0][k].Integral()
   rate2 = h[1][k].Integral()
   rate3 = hQCD[k].Integral()
 
-  datacard.write('{:>12.3f} {:>12.3f} {:>12.3f}'.format(rate1, rate2, rate3))
+  datacard.write("{:>12.3f} {:>12.3f} {:>12.3f}".format(rate1, rate2, rate3))
 
-datacard.write("\n")
+datacard.write(7*12*"-" + "\n")
+datacard.write("{:<8} {:<4}".format("QCD_Norm", "lnU"))
+for k in xrange(NCAT):
+  datacard.write("{:>12} {:>12} {:>12}".format("-", "-", "1.5\n"))
+
+datacard.write("{:<8} {:<4}".format("TTJ_Norm", "lnU"))
+for k in xrange(NCAT):
+  datacard.write("{:>12} {:>12} {:>12}".format("-", "1.5", "-\n"))
+
 datacard.close()
